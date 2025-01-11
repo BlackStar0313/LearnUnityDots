edb07a30740c0b4f24c118ba51af15493ccdfd21
zhiwei.liu <q270274978@gmail.com>
2025-01-11 14:15:05 +0800

    增加根据物理碰撞消失shell

--- Assets/Scripts/Tank/System/TankShellMoveSystem.cs
+++ Assets/Scripts/Tank/System/TankShellMoveSystem.cs
@@ -1,56 +1,114 @@
+using Unity.Jobs;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Transforms;
-
+using Unity.Physics.Systems;
+using Unity.Physics;
+using Unity.Logging;
 
 namespace Tank
 {
+    [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+    [UpdateAfter(typeof(PhysicsSystemGroup))]
     partial struct TankShellMoveSystem : ISystem
     {
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
             state.RequireForUpdate<TankShell>();
+            state.RequireForUpdate<PhysicsWorldSingleton>();
+            state.RequireForUpdate<SimulationSingleton>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+
+            // var ecb = new EntityCommandBuffer(Allocator.TempJob);
+            // var destroyJob = new TankShellDestoryJob
+            // {
+            //     EntityCommandBuffer = ecb.AsParallelWriter(),
+            //     BoomPosCollection = state.GetBufferLookup<TankShellBoomPosCollection>(),
+            //     ConfigEntity = SystemAPI.GetSingletonEntity<TankConfigData>(),
+            // };
+            // state.Dependency = destroyJob.Schedule(state.Dependency);
+            // state.Dependency.Complete();
+            // ecb.Playback(state.EntityManager);
+            // ecb.Dispose();
+
+            var simulation = SystemAPI.GetSingleton<SimulationSingleton>();
             var ecb = new EntityCommandBuffer(Allocator.TempJob);
-            var destroyJob = new TankShellDestoryJob
+
+            state.Dependency = new TankShellCollisionEvents
             {
-                EntityCommandBuffer = ecb.AsParallelWriter(),
-                BoomPosCollection = state.GetBufferLookup<TankShellBoomPosCollection>(),
+                ECB = ecb,
+                BoomPosLookup = state.GetBufferLookup<TankShellBoomPosCollection>(),
                 ConfigEntity = SystemAPI.GetSingletonEntity<TankConfigData>(),
-            };
-            state.Dependency = destroyJob.Schedule(state.Dependency);
+                TankShellLookup = SystemAPI.GetComponentLookup<TankShell>(),
+                TransformLookup = SystemAPI.GetComponentLookup<LocalTransform>()
+            }.Schedule(simulation, state.Dependency);
+
             state.Dependency.Complete();
             ecb.Playback(state.EntityManager);
             ecb.Dispose();
         }
-
     }
 
     [BurstCompile]
-    partial struct TankShellDestoryJob : IJobEntity
+    partial struct TankShellCollisionEvents : ICollisionEventsJob
     {
-        public EntityCommandBuffer.ParallelWriter EntityCommandBuffer;
-        public BufferLookup<TankShellBoomPosCollection> BoomPosCollection;
+        public EntityCommandBuffer ECB;
+        public BufferLookup<TankShellBoomPosCollection> BoomPosLookup;
         public Entity ConfigEntity;
+        public ComponentLookup<TankShell> TankShellLookup;
+        public ComponentLookup<LocalTransform> TransformLookup;
+        // public PhysicsWorld PhysicsWorld;
 
-        public void Execute(Entity entity, [EntityIndexInQuery] int index, [ReadOnly] ref LocalToWorld localToWorld, [ReadOnly] ref TankShell tankShell)
+        public void Execute(CollisionEvent collisionEvent)
         {
-            if (localToWorld.Position.y <= 0.5)
+            Entity entityA = collisionEvent.EntityA;
+            Entity entityB = collisionEvent.EntityB;
+
+            Log.Info("TankShellCollisionEvents Execute");
+            bool isShellA = TankShellLookup.HasComponent(entityA);
+            bool isShellB = TankShellLookup.HasComponent(entityB);
+
+            if (isShellA ^ isShellB)
             {
+                Entity shellEntity = isShellA ? entityA : entityB;
+                var shellPosition = TransformLookup[shellEntity].Position;
 
-                var buffer = BoomPosCollection[ConfigEntity];
-                buffer.Add(new TankShellBoomPosCollection { Position = localToWorld.Position });
+                var buffer = BoomPosLookup[ConfigEntity];
+                buffer.Add(new TankShellBoomPosCollection
+                {
+                    Position = shellPosition
+                });
 
-                EntityCommandBuffer.DestroyEntity(index, entity);
+                ECB.DestroyEntity(shellEntity);
             }
         }
     }
+
+    // [BurstCompile]
+    // partial struct TankShellDestoryJob : IJobEntity
+    // {
+    //     public EntityCommandBuffer.ParallelWriter EntityCommandBuffer;
+    //     public BufferLookup<TankShellBoomPosCollection> BoomPosCollection;
+    //     public Entity ConfigEntity;
+
+    //     public void Execute(Entity entity, [EntityIndexInQuery] int index, [ReadOnly] ref LocalToWorld localToWorld, [ReadOnly] ref TankShell tankShell)
+    //     {
+    //         if (localToWorld.Position.y <= 0.5)
+    //         {
+
+    //             var buffer = BoomPosCollection[ConfigEntity];
+    //             buffer.Add(new TankShellBoomPosCollection { Position = localToWorld.Position });
+
+    //             EntityCommandBuffer.DestroyEntity(index, entity);
+    //         }
+    //     }
+    // }
 
     // [WithAll(typeof(TankShell))]
     // public partial struct TankShellMoveJob : IJobEntity
